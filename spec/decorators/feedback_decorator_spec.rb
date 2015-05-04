f5fe3deca84aa72b7e20ecbcee0584bc5ad4f8ab
Feng Ye <feng.ye@youtiao.im>
2015-05-04 17:21:49 +0800

    add decorator specs

---
+++ spec/decorators/feedback_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe FeedbackDecorator, type: :decorator do
+end
---
+++ spec/decorators/post_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe PostDecorator, type: :decorator do
+end
