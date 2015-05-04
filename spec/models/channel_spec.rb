9adb6423e5ea71832c9a76b394a45d95164c0ffb
Feng Ye <feng.ye@youtiao.im>
2015-05-04 14:52:08 +0800

    fix typo

--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Channel, type: :model do
   it { should have_many :channel_users }
   it { should have_many(:users).through(:channel_users) }
-  it { should have_may :posts }
+  it { should have_many :posts }
 end
