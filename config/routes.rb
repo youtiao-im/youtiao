21272ddbc4ff1caf30b4dd1136f3cb9dfaceb28a
Feng Ye <feng.ye@youtiao.im>
2015-05-05 14:46:18 +0800

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   use_doorkeeper
   devise_for :users
+
+  root to: 'static_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
