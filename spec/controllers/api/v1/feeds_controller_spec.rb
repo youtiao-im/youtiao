c7a3b792a349b0b1c8c96ad37b9bb04b9d90f81a
fengye87 <yf0807@gmail.com>
2015-05-15 17:57:44 +0800

    fix pagination specs

--- spec/controllers/api/v1/feeds_controller_spec.rb
+++ spec/controllers/api/v1/feeds_controller_spec.rb
@@ -52,8 +52,8 @@ RSpec.describe Api::V1::FeedsController, type: :controller do
 
           it 'sets pagination headers' do
             get :index, channel_id: channel.to_param
-            expect(response.headers['Total']).to_not be_nil
-            expect(response.headers['Per-Page']).to_not be_nil
+            expect(response.headers['X-Total']).to_not be_nil
+            expect(response.headers['X-Per-Page']).to_not be_nil
           end
         end
       end
