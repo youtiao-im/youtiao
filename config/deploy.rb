439d89dc3e5e2e43868b45735482af181f7683dc
fengye87 <yf0807@gmail.com>
2015-07-09 17:30:34 +0800

    change application.css -> application.css.scss

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,4 +53,12 @@ namespace :deploy do
     end
   end
 
+  before :compile_assets, :bower_install do
+    on roles(:web) do
+      within release_path do
+        execute :rake, 'bower:install CI=true'
+      end
+    end
+  end
+
 end
