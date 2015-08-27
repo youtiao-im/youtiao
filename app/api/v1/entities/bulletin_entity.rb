5dc64b3281aa7c36523c5cf1cfcc10e3ebe9a868
fengye87 <yf0807@gmail.com>
2015-08-27 13:53:47 +0800

    fix eyes_count return

--- app/api/v1/entities/bulletin_entity.rb
+++ app/api/v1/entities/bulletin_entity.rb
@@ -1,7 +1,7 @@
 module V1
   module Entities
     class BulletinEntity < BaseEntity
-      expose :text, :checks_count, :crosses_count, :comments_count
+      expose :text, :checks_count, :crosses_count, :eyes_count, :comments_count
       expose :group_id do |bulletin|
         Group.encrypt_id(bulletin.group_id)
       end
